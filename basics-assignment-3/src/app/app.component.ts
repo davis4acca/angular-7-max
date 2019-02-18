93c2b1b366b39322947f191972cecd95c31377be
davis4acca@gmail.com <davis4acca@gmail.com>
2019-02-18 16:48:22 +0000

    update

--- basics-assignment-3/src/app/app.component.ts
+++ basics-assignment-3/src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+let idGen = (function* idGen() {
+  let id = 0;
+
+  while (true) {
+    id++;
+    yield id;
+  }
+})();
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  isPasswordVisible = true;
+  buttonClickLogList = [];
+
+  togglePassword() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+    this.buttonClickLogList.push(idGen.next().value);
+
+    console.log(this.buttonClickLogList);
+  }
 }
